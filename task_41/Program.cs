7a0f0bae85054e4b31e4089e872910c0961ea00b
AnnaKim <annakim@inbox.ru>
2023-01-25 18:14:05 +0300

    task_41

---
+++ task_41/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+int [] GetUserArray (int Length)
+{
+    int [] array = new int [Length];
+    for (int i = 0;i < Length; i ++)
+    {
+         array [i] = new Random().Next(99, 1000);
+    }
+    return array;
+}
+int [] Array = GetUserArray (5); 
+
+void ArrayOutput (int [] Array)
+{
+    Console.Write ($"[");
+    for(int i = 0; i <Array.Length; i ++)
+    {
+        if (i != Array.Length-1)
+        {
+            Console.Write($"{Array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{Array[i]}]");
+        }
+    }
+}
+ArrayOutput (Array);
